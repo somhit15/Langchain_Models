3b891810727294d8a1bcd1760c5b42ec45e29cd6
Somnath Dutta <somhit15@gmail.com>
2026-02-23 12:07:02 +0530

    added anthropic model

---
+++ ChatModels/2_chatmodel_anthropic.py
@@ -0,0 +1,12 @@
+from langchain_anthropic import ChatAnthropic
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatAnthropic(model="claude-sonnet-4-6")
+
+result = model.invoke("What is the capital of Jharkhand?")
+
+print(result)
+
+
